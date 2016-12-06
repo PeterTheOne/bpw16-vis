ce9587b717277e8cc05a36cd989a9104f48db8b9
PeterTheOne <petertheone@gmail.com>
2016-12-06 07:59:18 +0100

    add more data

--- js/script.js
+++ js/script.js
@@ -387,6 +387,443 @@
     }
   ];
 
+  var data_wg2_2 = [
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Graz(Stadt) Gesamt",
+      "Gültige": 145.122,
+      "Hofer": 47.825,
+      "Hofer %": 32.96,
+      "Van der Bellen": 97.297,
+      "Van der Bellen %": 67.04
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Graz Wahllokal",
+      "Gültige": 119.704,
+      "Hofer": 42.35,
+      "Hofer %": 35.38,
+      "Van der Bellen": 77.354,
+      "Van der Bellen %": 64.62
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Graz(Stadt)",
+      "Gültige": 25.418,
+      "Hofer": 5.475,
+      "Hofer %": 21.54,
+      "Van der Bellen": 19.943,
+      "Van der Bellen %": 78.46
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Deutschlandsberg Gesamt",
+      "Gültige": 35.793,
+      "Hofer": 22.043,
+      "Hofer %": 61.58,
+      "Van der Bellen": 13.75,
+      "Van der Bellen %": 38.42
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Deutschlandsberg",
+      "Gültige": 30.091,
+      "Hofer": 19.195,
+      "Hofer %": 63.79,
+      "Van der Bellen": 10.896,
+      "Van der Bellen %": 36.21
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Deutschlandsberg",
+      "Gültige": 5.702,
+      "Hofer": 2.848,
+      "Hofer %": 49.95,
+      "Van der Bellen": 2.854,
+      "Van der Bellen %": 50.05
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Graz-Umgebung Gesamt",
+      "Gültige": 86.901,
+      "Hofer": 45.989,
+      "Hofer %": 52.92,
+      "Van der Bellen": 40.912,
+      "Van der Bellen %": 47.08
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Graz-Umgebung",
+      "Gültige": 74.304,
+      "Hofer": 40.638,
+      "Hofer %": 54.69,
+      "Van der Bellen": 33.666,
+      "Van der Bellen %": 45.31
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Graz-Umgebung",
+      "Gültige": 12.597,
+      "Hofer": 5.351,
+      "Hofer %": 42.48,
+      "Van der Bellen": 7.246,
+      "Van der Bellen %": 57.52
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Leibnitz Gesamt",
+      "Gültige": 45.929,
+      "Hofer": 29.662,
+      "Hofer %": 64.58,
+      "Van der Bellen": 16.267,
+      "Van der Bellen %": 35.42
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Leibnitz",
+      "Gültige": 39.986,
+      "Hofer": 26.547,
+      "Hofer %": 66.39,
+      "Van der Bellen": 13.439,
+      "Van der Bellen %": 33.61
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Leibnitz",
+      "Gültige": 5.943,
+      "Hofer": 3.115,
+      "Hofer %": 52.41,
+      "Van der Bellen": 2.828,
+      "Van der Bellen %": 47.59
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Leoben",
+      "Gültige": 34.112,
+      "Hofer": 18.798,
+      "Hofer %": 55.11,
+      "Van der Bellen": 15.314,
+      "Van der Bellen %": 44.89
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Leoben",
+      "Gültige": 29.404,
+      "Hofer": 16.963,
+      "Hofer %": 57.69,
+      "Van der Bellen": 12.441,
+      "Van der Bellen %": 42.31
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Leoben",
+      "Gültige": 4.708,
+      "Hofer": 1.835,
+      "Hofer %": 38.98,
+      "Van der Bellen": 2.873,
+      "Van der Bellen %": 61.02
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Liezen",
+      "Gültige": 42.266,
+      "Hofer": 22.457,
+      "Hofer %": 53.13,
+      "Van der Bellen": 19.809,
+      "Van der Bellen %": 46.87
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Liezen",
+      "Gültige": 35.269,
+      "Hofer": 19.682,
+      "Hofer %": 55.81,
+      "Van der Bellen": 15.587,
+      "Van der Bellen %": 44.19
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Liezen",
+      "Gültige": 6.997,
+      "Hofer": 2.775,
+      "Hofer %": 39.66,
+      "Van der Bellen": 4.222,
+      "Van der Bellen %": 60.34
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Murau",
+      "Gültige": 15.979,
+      "Hofer": 9.551,
+      "Hofer %": 59.77,
+      "Van der Bellen": 6.428,
+      "Van der Bellen %": 40.23
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Murau",
+      "Gültige": 13.414,
+      "Hofer": 8.331,
+      "Hofer %": 62.11,
+      "Van der Bellen": 5.083,
+      "Van der Bellen %": 37.89
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Murau",
+      "Gültige": 2.565,
+      "Hofer": 1.22,
+      "Hofer %": 47.56,
+      "Van der Bellen": 1.345,
+      "Van der Bellen %": 52.44
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Voitsberg",
+      "Gültige": 30.637,
+      "Hofer": 19.08,
+      "Hofer %": 62.28,
+      "Van der Bellen": 11.557,
+      "Van der Bellen %": 37.72
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Voitsberg",
+      "Gültige": 26.993,
+      "Hofer": 17.317,
+      "Hofer %": 64.15,
+      "Van der Bellen": 9.676,
+      "Van der Bellen %": 35.85
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Voitsberg",
+      "Gültige": 3.644,
+      "Hofer": 1.763,
+      "Hofer %": 48.38,
+      "Van der Bellen": 1.881,
+      "Van der Bellen %": 51.62
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Weiz",
+      "Gültige": 51.81,
+      "Hofer": 29.253,
+      "Hofer %": 56.46,
+      "Van der Bellen": 22.557,
+      "Van der Bellen %": 43.54
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Weiz",
+      "Gültige": 46.015,
+      "Hofer": 26.786,
+      "Hofer %": 58.21,
+      "Van der Bellen": 19.229,
+      "Van der Bellen %": 41.79
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Weiz",
+      "Gültige": 5.795,
+      "Hofer": 2.467,
+      "Hofer %": 42.57,
+      "Van der Bellen": 3.328,
+      "Van der Bellen %": 57.43
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Murtal",
+      "Gültige": 40.427,
+      "Hofer": 22.736,
+      "Hofer %": 56.24,
+      "Van der Bellen": 17.691,
+      "Van der Bellen %": 43.76
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Murtal",
+      "Gültige": 34.458,
+      "Hofer": 20.219,
+      "Hofer %": 58.68,
+      "Van der Bellen": 14.239,
+      "Van der Bellen %": 41.32
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Murtal",
+      "Gültige": 5.969,
+      "Hofer": 2.517,
+      "Hofer %": 42.17,
+      "Van der Bellen": 3.452,
+      "Van der Bellen %": 57.83
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Bruck-Mürzzuschlag",
+      "Gültige": 55.804,
+      "Hofer": 29.07,
+      "Hofer %": 52.09,
+      "Van der Bellen": 26.734,
+      "Van der Bellen %": 47.91
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Bruck-Mürzzuschlag",
+      "Gültige": 46.963,
+      "Hofer": 25.517,
+      "Hofer %": 54.33,
+      "Van der Bellen": 21.446,
+      "Van der Bellen %": 45.67
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Bruck-Mürzzuschlag",
+      "Gültige": 8.841,
+      "Hofer": 3.553,
+      "Hofer %": 40.19,
+      "Van der Bellen": 5.288,
+      "Van der Bellen %": 59.81
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Hartberg-Fürstenfeld",
+      "Gültige": 55.362,
+      "Hofer": 35.957,
+      "Hofer %": 64.95,
+      "Van der Bellen": 19.405,
+      "Van der Bellen %": 35.05
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Hartberg-Fürstenfeld",
+      "Gültige": 49.701,
+      "Hofer": 33.157,
+      "Hofer %": 66.71,
+      "Van der Bellen": 16.54,
+      "Van der Bellen %": 33.28
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Hartberg-Fürstenfeld",
+      "Gültige": 5.661,
+      "Hofer": 2.8,
+      "Hofer %": 49.46,
+      "Van der Bellen": 2.865,
+      "Van der Bellen %": 50.61
+    },
+    {
+      "Grafiknr": 3,
+      "Gebietsname": "Südoststeiermark",
+      "Gültige": 49.596,
+      "Hofer": 31.357,
+      "Hofer %": 63.22,
+      "Van der Bellen": 18.239,
+      "Van der Bellen %": 36.78
+    },
+    {
+      "Grafiknr": 1,
+      "Gebietsname": "Wahllokal Südoststeiermark",
+      "Gültige": 43.548,
+      "Hofer": 28.286,
+      "Hofer %": 64.95,
+      "Van der Bellen": 15.262,
+      "Van der Bellen %": 35.05
+    },
+    {
+      "Grafiknr": 2,
+      "Gebietsname": "Wahlkarten - Südoststeiermark",
+      "Gültige": 6.048,
+      "Hofer": 3.071,
+      "Hofer %": 50.78,
+      "Van der Bellen": 2.977,
+      "Van der Bellen %": 49.22
+    }
+  ];
+
+  function colorMaps(file, startId) {
+    $(file).each(function (index, value){
+      var id = startId;
+      switch (value['Grafiknr']) {
+        case 3:
+          id += 'gesamt_';
+          break;
+        case 1:
+          id += 'lokal_';
+          break;
+        case 2:
+          id += 'briefwahl_';
+          break;
+      }
+
+      var name = value['Gebietsname'];
+      name = name.toLowerCase();
+      name = name.replace('gesamt', '');
+      name = name.replace('wahlkarten', '');
+      name = name.replace('wahllokal', '');
+      name = name.replace('(stadt)', '');
+      name = name.replace(' - ', ' ');
+      name = name.trim();
+
+      var $mapHofer = $(id + 'hofer_map');
+      var $mapVdb = $(id + 'vdb_map');
+
+      var hoferValue = value['Hofer %'];
+      var vdbValue = value['Van der Bellen %'];
+
+      hoferValue -= 20;
+      vdbValue -= 20;
+
+      hoferValue *= 2; // range from 20 to 70 is 50 of 100 long.
+      vdbValue *= 2;
+
+      hoferValue /= 20;
+      vdbValue /= 20;
+
+      hoferValue = Math.floor(hoferValue);
+      vdbValue = Math.floor(vdbValue);
+
+      hoferValue *= 20;
+      vdbValue *= 20;
+
+      hoferValue /= 100;
+      vdbValue /= 100;
+
+      hoferValue = hoferValue > 1 ? 1 : hoferValue;
+      vdbValue = vdbValue > 1 ? 1 : vdbValue;
+
+      hoferValue = hoferValue < 0 ? 0 : hoferValue;
+      vdbValue = vdbValue < 0 ? 0 : vdbValue;
+
+      console.log(hoferValue);
+      console.log(vdbValue);
+
+      $('.' + name, $mapHofer)
+        .attr('fill', 'blue')
+        .attr('fill-opacity', hoferValue)
+        .html($('<title>').text(value['Gebietsname'] + ' Hofer: ' + value['Hofer %'] + '%'));
+      $('.' + name, $mapVdb)
+        .attr('fill', 'green')
+        .attr('fill-opacity', vdbValue)
+        .html($('<title>').text(value['Gebietsname'] + ' VDB: ' + value['Van der Bellen %'] + '%'));
+
+
+      $('.legend-01', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.0);
+      $('.legend-02', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.2);
+      $('.legend-03', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.4);
+      $('.legend-04', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.6);
+      $('.legend-05', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.8);
+      $('.legend-06', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 1.0);
+
+      $('.legend-01', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.0);
+      $('.legend-02', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.2);
+      $('.legend-03', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.4);
+      $('.legend-04', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.6);
+      $('.legend-05', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.8);
+      $('.legend-06', $mapVdb).attr('fill', 'green').attr('fill-opacity', 1.0);
+    });
+  }
+
   $(function () {
 
     inlineSvg();
@@ -394,98 +831,24 @@
     // todo: don't use timeout
     setTimeout(function () {
 
-      $(data_wg2_1).each(function (index, value){
-        var id = '#wg2_1_';
-        switch (value['Grafiknr']) {
-          case 3:
-            id += 'gesamt_';
-            break;
-          case 1:
-            id += 'lokal_';
-            break;
-          case 2:
-            id += 'briefwahl_';
-            break;
-        }
-
-        var name = value['Gebietsname'];
-        name = name.toLowerCase();
-        name = name.replace('gesamt', '');
-        name = name.replace('wahlkarten', '');
-        name = name.replace('wahllokal', '');
-        name = name.replace('(stadt)', '');
-        name = name.replace(' - ', ' ');
-        name = name.trim();
-
-        var $mapHofer = $(id + 'hofer_map');
-        var $mapVdb = $(id + 'vdb_map');
-
-        var hoferValue = value['Hofer %'];
-        var vdbValue = value['Van der Bellen %'];
-
-        hoferValue -= 20;
-        vdbValue -= 20;
-
-        hoferValue *= 2; // range from 20 to 70 is 50 of 100 long.
-        vdbValue *= 2;
-
-        hoferValue /= 20;
-        vdbValue /= 20;
-
-        hoferValue = Math.floor(hoferValue);
-        vdbValue = Math.floor(vdbValue);
-
-        hoferValue *= 20;
-        vdbValue *= 20;
-
-        hoferValue /= 100;
-        vdbValue /= 100;
-
-        hoferValue = hoferValue > 1 ? 1 : hoferValue;
-        vdbValue = vdbValue > 1 ? 1 : vdbValue;
-
-        hoferValue = hoferValue < 0 ? 0 : hoferValue;
-        vdbValue = vdbValue < 0 ? 0 : vdbValue;
-
-        console.log(hoferValue);
-        console.log(vdbValue);
-
-        $('.' + name, $mapHofer)
-          .attr('fill', 'blue')
-          .attr('fill-opacity', hoferValue)
-          .html($('<title>').text(value['Gebietsname'] + ' Hofer: ' + value['Hofer %'] + '%'));
-        $('.' + name, $mapVdb)
-          .attr('fill', 'green')
-          .attr('fill-opacity', vdbValue)
-          .html($('<title>').text(value['Gebietsname'] + ' VDB: ' + value['Van der Bellen %'] + '%'));
-
-
-        $('.legend-01', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.0);
-        $('.legend-02', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.2);
-        $('.legend-03', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.4);
-        $('.legend-04', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.6);
-        $('.legend-05', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 0.8);
-        $('.legend-06', $mapHofer).attr('fill', 'blue').attr('fill-opacity', 1.0);
-
-        $('.legend-01', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.0);
-        $('.legend-02', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.2);
-        $('.legend-03', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.4);
-        $('.legend-04', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.6);
-        $('.legend-05', $mapVdb).attr('fill', 'green').attr('fill-opacity', 0.8);
-        $('.legend-06', $mapVdb).attr('fill', 'green').attr('fill-opacity', 1.0);
-      });
+      colorMaps(data_wg2_1, '#wg2_1_');
+      colorMaps(data_wg2_2, '#wg2_2_');
 
       var $hoferMapBriefwahl = $('#wg2_1_briefwahl_hofer_map');
       var $vdbMapBriefwahl = $('#wg2_1_briefwahl_vdb_map');
 
       $('.legend-01', $hoferMapBriefwahl).show();
-      $('.legend-06', $hoferMapBriefwahl).show();
       $('.legend-01-text', $hoferMapBriefwahl).show();
-      $('.legend-06-text', $hoferMapBriefwahl).show();
-      $('.legend-01', $vdbMapBriefwahl).show();
       $('.legend-06', $vdbMapBriefwahl).show();
-      $('.legend-01-text', $vdbMapBriefwahl).show();
       $('.legend-06-text', $vdbMapBriefwahl).show();
+
+      var $hoferMapBriefwahl2 = $('#wg2_2_briefwahl_hofer_map');
+      var $vdbMapBriefwahl2 = $('#wg2_2_briefwahl_vdb_map');
+
+      $('.legend-01', $hoferMapBriefwahl2).show();
+      $('.legend-01-text', $hoferMapBriefwahl2).show();
+      $('.legend-06', $vdbMapBriefwahl2).show();
+      $('.legend-06-text', $vdbMapBriefwahl2).show();
 
       var $maps = $('.map');
 
